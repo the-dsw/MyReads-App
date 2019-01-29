f1ef3c8fe8f132611432ccb24cd1248f3e34371c
mauricio oporto <fmod@the-dsw.com>
2019-01-29 12:20:18 +0100

    fix: search input changes are not reflected instantly

--- src/SearchBook.js
+++ src/SearchBook.js
@@ -10,28 +10,37 @@ class SearchBook extends React.Component {
     error: ''
   }
 
-  handleInputChange = () => {
-    this.setState(() => ({
-      query: this.search.value
-    }));
+  handleInputChange = (query) => {
+    this.setState({
+      query
+    });
    
-    if (this.state.query && this.state.query.length > 1) {
-      this.getSearchBooks();
+    if (query) {
+      this.getSearchBooks(query);
+    } else {
+      this.cleanSearchBook();
     }
   }     
- 
-  getSearchBooks = () => {
-    BooksAPI.search(this.state.query)
+  
+  cleanSearchBook = () => {
+    this.setState({
+      searchBooks: [],
+      error: ''
+    });
+  }
+
+  getSearchBooks = (query) => {
+    BooksAPI.search(query)
       .then((searchBooks) => {
         if(!searchBooks.error) {
-          this.setState(() => ({
+          this.setState({
             searchBooks,
             error: ''
-          }));
+          });
         } else {
-          this.setState(() => ({
+          this.setState({
             error: searchBooks.error
-          }));
+          });
         }
         
       })
@@ -42,28 +51,26 @@ class SearchBook extends React.Component {
     const { error, query } = this.state;
     const { moveBook } = this.props;
   
-
     return (
       <div className="search-books">
         <div className="search-books-bar">
-          <Link to="/" onClick={() => (console.log("clicked"))} className="close-search" >Close</Link>
+          <Link to="/" className="close-search" >Close</Link>
           <div className="search-books-input-wrapper">
             <form>
               <input 
                 type="text" 
                 placeholder="Search by title or author"
                 ref={input => this.search = input}
-                onChange={this.handleInputChange} />
+                onChange={(e) => this.handleInputChange(this.search.value)} />
             </form>
 
           </div>
         </div>
         <div className="search-books-results">
           <ol className="books-grid">
-            {error === 'empty query' ? (
+            {error ? (
               <p style={{color: 'red'}}>{`Any Book with ${query}`}</p>
-            ) : (<Suggestions moveBook={moveBook} results={this.state.searchBooks} />)}
-         
+            ) : query && (<Suggestions moveBook={moveBook} results={this.state.searchBooks} />)}
           </ol>
         </div>
       </div>
